16d9f058de6e24c17c70bbff6e77df3f14162bf7
Faical Yannick Congo <yannick.congo@gmail.com>
2019-06-03 11:21:51 +0200

    Comments and fixing up origin url.

--- fix-missing.sh
+++ fix-missing.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# This script help overcome git/gitsubmodule constrains
+# - Genuine upstream information is lost, which is annoying.
+# - Base repo and submodules origins are not alined.
+
+# Solutions:
+# - Fetch preserved upstream from one of the submodules (web) and set it up in base.
+# - Update the origins and upstreams for all submodules.
+
+# This repository is mixing two features.
+# - Git submodule: reference other git repository in one for dev purposes.
+# - The web container is built from the apps container which allow two stage development.
+# - Docker-compose healthy check which allow health check deployments.
+
+
 submodules="apps/blob_utils apps/core_composer_app apps/core_curate_app apps/core_dashboard_app apps/core_dashboard_common_app "
 submodules=${submodules}"apps/core_explore_common_app apps/core_explore_example_app apps/core_explore_federated_search_app "
 submodules=${submodules}"apps/core_explore_keyword_app apps/core_explore_oaipmh_app apps/core_exporters_app apps/core_federated_search_app "
@@ -7,9 +21,11 @@ submodules=${submodules}"apps/core_main_app apps/core_module_blob_host_app apps/
 submodules=${submodules}"apps/core_module_periodic_table_app apps/core_module_text_area_app apps/core_oaipmh_common_app apps/core_oaipmh_harvester_app "
 submodules=${submodules}"apps/core_oaipmh_provider_app apps/core_parser_app apps/core_website_app apps/signals_utils apps/xml_utils"
 
+cd web
 echo "setting up upstream..."
 git remote add upstream https://github.com/faical-yannick-congo/MDCS-DEV.git
 echo "...upstream set"
+cd ..
 
 echo "fetching origin..."
 origin=`git config --get remote.origin.url`
